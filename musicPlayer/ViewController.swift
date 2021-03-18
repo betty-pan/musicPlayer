a11150f628c843aeea0eee16314ab25e280962cd
Betty Pan <betty19931218@gmail.com>
2021-03-19 00:32:44 +0800

    Initial Commit

---
+++ musicPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  musicPlayer
+//
+//  Created by Betty Pan on 2021/3/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
